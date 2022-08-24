54a05e6b036ea9af137774fb12c098cf0c2467cb
Zhucong Xi <zhucongx@umich.edu>
2022-08-23 23:41:08 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 
 #include "MonteCarlo.h"
 int main(int argc, char *argv[]) {
-  // Factor_t factor;
-  // if (argc != 4) {
-  //   std::cout << "No input ." << std::endl;
-  //   return 1;
-  // } else {
-  //   for (int i = 1; i < argc; i++) {
-  //     factor[i-1] = std::stoull(argv[i]);
-  //   }
-  // }
+  Factor_t factor;
+  if (argc != 4) {
+    std::cout << "No input ." << std::endl;
+    return 1;
+  } else {
+    for (int i = 1; i < argc; i++) {
+      factor[i-1] = std::stoull(argv[i]);
+    }
+  }
 
-  MonteCarlo monte_carlo({24, 24, 24},
+  MonteCarlo monte_carlo(factor,
                          1e3,
                          1e4,
                          1e10);
